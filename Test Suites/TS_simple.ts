845c8adcc131c2c4abcd15fbc6ff0d9b42d92962
hani.vo <hani.vo@localhost>
2024-02-08 10:22:07 +0700

    update 8Feb

--- Test Suites/TS_simple.ts
+++ Test Suites/TS_simple.ts
@@ -5,10 +5,23 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>01529ef0-2718-4ae2-b973-828566b486ab</testSuiteGuid>
+   <testCaseLink>
+      <guid>ec568ff2-59a3-42ee-b025-08b36efd282c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AutomationExcercise/TC1_Register</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>0d52e68d-b918-454a-b3ec-c1f65f030cd0</variableId>
+      </variableLink>
+   </testCaseLink>
 </TestSuiteEntity>
